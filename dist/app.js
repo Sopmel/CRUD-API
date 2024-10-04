8d142dd8e1d9d281162db02c2e6844f48872feb0
Sopmel <soph.melin@gmail.com>
2024-10-04 21:52:16 +0200

    first commit

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+app.get('*', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
